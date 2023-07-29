ecada6a9dbf17bf893f59cc1cbc33ba683814038
aiotuy138m <122207424+aiotuy138m@users.noreply.github.com>
2023-07-29 14:27:50 +0000

    [wip]コントローラーのテストの記述

---
+++ rspec/spec/system/lists_spec.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe '投稿テスト' do
+  let! (:list) { create(:list, title:'test', body:'testtest', img:'app/assets/images/no-icon.png') }
+  discribe 'トップ画面(top_path)のテスト' do
+    
+  end
+end
