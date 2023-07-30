4c295ca62a0523cbfb32852301bde1a47d9c1bb3
aiotuy138m <122207424+aiotuy138m@users.noreply.github.com>
2023-07-30 10:29:55 +0000

    [finish]トップ画面のテスト

--- rspec/spec/system/lists_spec.rb
+++ rspec/spec/system/lists_spec.rb
@@ -5,6 +5,16 @@ require 'rails_helper'
 describe '投稿テスト' do
   let! (:list) { create(:list, title:'test', body:'testtest', img:'app/assets/images/no-icon.png') }
   discribe 'トップ画面(top_path)のテスト' do
-    
+    before 'top画面への遷移' do
+      redirect_to '/top'
+    end
+    context '表示の確認' do
+      it 'トップ画面(top_path)に「ここはTopページです」が表示されているか' do
+        expect(page).to have_content 'ここはTopページです'
+      end
+      it 'top_pathが"/top"であるか' do
+        expect(page).to have_current_path '/top'
+      end
+    end
   end
 end
