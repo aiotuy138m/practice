c05e0e98804845b876fff3befd3371af9d3aaa48
aiotuy138m <122207424+aiotuy138m@users.noreply.github.com>
2023-08-01 14:00:02 +0000

    [finish]Java2章

--- Java/Chap2.java
+++ Java/Chap2.java
@@ -14,5 +14,32 @@ public class Chap2 {
     System.out.println(d);
     int age = (int)3.2; //小数をintに型変換して代入
     System.out.println(age);
+    double dou = 8.5 / 2; //異なる型同士の算術演算
+    long l = 5 + 2L;
+    System.out.println(dou);
+    System.out.println(l);
+    String msg = "私の年齢は" + 23;
+    System.out.println(msg);
+    String name = "すがわら"; //変数宣言の文
+    String message; //変数宣言の文
+    message = name + "さん、こんにちは"; //計算の文
+    System.out.println(message); //命令実行の文
+    System.out.print("私の名前は"); //改行しない
+    System.out.print(name); //改行しない
+    System.out.print("です"); //改行しない
+    int e = 5;
+    int g = 3;
+    int m = Math.max(e, g); //eとg大きい方が代入される
+    System.out.println("比較実験：" + e + "と" + g + "とで大きい方は…" + m);
+    String age2 = "31";
+    int n = Integer.parseInt(age2); //文字列を数値に変換する
+    System.out.println("あなたは来年、" + (n + 1) + "歳になりますね。");
+    int r = new java.util.Random().nextInt(90); //ランダム
+    System.out.println("あなたはたぶん、" + r + "歳ですね？");
+    System.out.println("あなたの名前を入力してください。");
+    String name2 = new java.util.Scanner(System.in).nextLine(); //文字列を入力
+    System.out.println("あなたの年齢を入力してください。");
+    int age3 = new java.util.Scanner(System.in).nextInt(); //数値を入力
+    System.out.println("ようこそ、" + age3 + "歳の" + name2 + "さん");
   }
 }
