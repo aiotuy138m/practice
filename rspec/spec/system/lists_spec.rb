7119237802f5f274644ecf1cee1fae587e5c4219
aiotuy138m <122207424+aiotuy138m@users.noreply.github.com>
2023-08-03 13:44:36 +0000

    [update]投稿画面のテスト＆[fix]投稿テスト

--- rspec/spec/system/lists_spec.rb
+++ rspec/spec/system/lists_spec.rb
@@ -7,14 +7,14 @@ Rspec.describe List, "コントローラーのテスト" do
     let! (:list) { create(:list, title:'test', body:'testtest', img:'app/assets/images/no-icon.png') }
     discribe 'トップ画面(top_path)のテスト' do
       before 'top画面への遷移' do
-        redirect_to '/top'
+        visit top_path
       end
       context '表示の確認' do
         it 'トップ画面(top_path)に「ここはTopページです」が表示されているか' do
           expect(page).to have_content 'ここはTopページです'
         end
         it 'top_pathが"/top"であるか' do
-          expect(page).to have_current_path '/top'
+          expect(current_path).to eq('/top')
         end
       end
     end
@@ -22,19 +22,22 @@ Rspec.describe List, "コントローラーのテスト" do
   
   describe '投稿画面のテスト' do
     before '投稿画面への遷移' do
-      redirect_to '/lists/new'
+      visit lists_new_path
     end
     context '表示の確認' do
       it 'new_list_pathが"/lists/new"であるか' do
-        
+        expect(current_path).to eq('/lists/new')
       end
       it '投稿ボタンが表示されているか' do
-        
+        expect(page).to have_button '投稿'
       end
     end
     context '投稿処理のテスト' do
       it '投稿後のリダイレクト先は正しいか' do
-        
+        fill_in 'list[title]', with: Facker::Lorem.characters(number:5)
+        fill_in 'list[body]', with: Facker::Lorem.characters(number:5)
+        click_button '投稿'
+        expect(page).to have_current_path book_path(Book.last)
       end
     end
   end
