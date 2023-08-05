8c54dc463fb70e10122b514eeedf614c5ed87062
aiotuy138m <122207424+aiotuy138m@users.noreply.github.com>
2023-08-05 14:03:16 +0000

    [wip]Rspec動作確認

--- rspec/spec/models/list_spec.rb
+++ rspec/spec/models/list_spec.rb
@@ -3,8 +3,9 @@
 require 'rails_helper'
 
 describe 'モデルのテスト' do
-  context '' do
-    it '有効な投稿内容の場合は保存されるか'
+  context 'モデルのテスト' do
+    it '有効な投稿内容の場合は保存されるか' do
       expect(FactoryBot.build(:list)).to be_valid  #Listモデルのインスタンスの作成,有効であるか
+    end
   end
 end
--- rspec/spec/system/lists_spec.rb
+++ rspec/spec/system/lists_spec.rb
@@ -2,10 +2,18 @@
 
 require 'rails_helper'
 
+
+FactoryBot.define do
+  factory :list do
+    title { Faker::Lorem.characters(number:10) }
+    body { Faker::Lorem.characters(number:30) }
+  end
+end
+
 Rspec.describe List, "コントローラーのテスト" do
   describe '投稿テスト' do
-    let!(:list) { create(:list, title:'test',body:'testtest') }  #img:'app/assets/images/no-icon.png'は不要？
-    discribe 'トップ画面(top_path)のテスト' do
+    let!(:list) { create(:list, title:'test', body:'testtest') }  #image:'app/assets/images/no-icon.png'は不要？
+    describe 'トップ画面(top_path)のテスト' do
       before 'top画面への遷移' do
         visit top_path
       end
@@ -35,7 +43,7 @@ Rspec.describe List, "コントローラーのテスト" do
         it '投稿後のリダイレクト先は正しいか' do
           fill_in 'list[title]', with: Facker::Lorem.characters(number:10) #number:5⇒10に修正
           fill_in 'list[body]', with: Facker::Lorem.characters(number:30) #number:20⇒30に修正
-          # fill_in 'list[img]'
+          # fill_in 'list[image]'
           click_button '投稿'
           expect(page).to have_current_path list_path(List.last)
         end
@@ -50,7 +58,7 @@ Rspec.describe List, "コントローラーのテスト" do
         it '一覧表示画面に投稿されたものが表示されているか' do
           expect(page).to have_content list.title
           expect(page).to have_content list.body
-          # expect(page).to have_content list.img
+          # expect(page).to have_content list.image
         end
       end
     end
@@ -72,9 +80,11 @@ Rspec.describe List, "コントローラーのテスト" do
         end
       end
       context 'リンクの遷移先は編集画面か' do
-        edit_link = find_all('a')[3] #1⇒3に修正
-        edit_link.click
-        expect(current_path).to eq('/lists/' + list.id.to_s + '/edit') #'/lists'から'/lists/'に修正
+        it '編集の遷移先は編集画面か' do
+          edit_link = find_all('a')[3] #1⇒3に修正
+          edit_link.click
+          expect(current_path).to eq('/lists/' + list.id.to_s + '/edit') #'/lists'から'/lists/'に修正
+        end
       end
       context 'list削除のテスト' do
         it 'listの削除' do
@@ -91,7 +101,7 @@ Rspec.describe List, "コントローラーのテスト" do
         it '編集前のタイトルと本文がフォームに表示（セット）されている' do
           expect(page).to have_filed 'list[title]', whth: list.title
           expect(page).to have_filed 'list[body]', whth: list.body
-          # expect(page).to have_filed 'list[body]', whth: list.img
+          # expect(page).to have_filed 'list[body]', whth: list.image
         end
         it '保存ボタンが表示される' do
           expect(page).to have_button '保存'
@@ -101,7 +111,7 @@ Rspec.describe List, "コントローラーのテスト" do
         it '更新後のリダイレクト先は正しいか' do
           fill_in 'list[itle]', with: Facker::Lorem.characters(number:10) #number:5⇒10に修正
           fill_in 'list[body]', with: Facker::Lorem.characters(number:30) #number:20⇒30に修正
-          # fill_in 'list[img]'
+          # fill_in 'list[image]'
           click_button '保存'
           expect(page).to have_current_path list_path(list) #List.last⇒listに修正
         end
