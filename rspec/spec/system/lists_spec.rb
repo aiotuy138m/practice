7dbc5215cd836104e1e8e529c8a78f1533e7168b
aiotuy138m <122207424+aiotuy138m@users.noreply.github.com>
2023-08-05 12:53:27 +0000

    [fix]教材との答え合わせ

--- rspec/spec/system/lists_spec.rb
+++ rspec/spec/system/lists_spec.rb
@@ -4,7 +4,7 @@ require 'rails_helper'
 
 Rspec.describe List, "コントローラーのテスト" do
   describe '投稿テスト' do
-    let! (:list) { create(:list, title:'test', body:'testtest', img:'app/assets/images/no-icon.png') }
+    let!(:list) { create(:list, title:'test',body:'testtest') }  #img:'app/assets/images/no-icon.png'は不要？
     discribe 'トップ画面(top_path)のテスト' do
       before 'top画面への遷移' do
         visit top_path
@@ -19,9 +19,9 @@ Rspec.describe List, "コントローラーのテスト" do
       end
     end
   
-    describe '投稿画面のテスト' do
+    describe '投稿画面(new_list_path)のテスト' do #(new_list_path)を追記
       before '投稿画面への遷移' do
-        visit lists_new_path
+        visit new_list_path #path記載ミス修正
       end
       context '表示の確認' do
         it 'new_list_pathが"/lists/new"であるか' do
@@ -33,9 +33,9 @@ Rspec.describe List, "コントローラーのテスト" do
       end
       context '投稿処理のテスト' do
         it '投稿後のリダイレクト先は正しいか' do
-          fill_in 'list[title]', with: Facker::Lorem.characters(number:5)
-          fill_in 'list[body]', with: Facker::Lorem.characters(number:20)
-          fill_in 'list[img]'
+          fill_in 'list[title]', with: Facker::Lorem.characters(number:10) #number:5⇒10に修正
+          fill_in 'list[body]', with: Facker::Lorem.characters(number:30) #number:20⇒30に修正
+          # fill_in 'list[img]'
           click_button '投稿'
           expect(page).to have_current_path list_path(List.last)
         end
@@ -50,7 +50,7 @@ Rspec.describe List, "コントローラーのテスト" do
         it '一覧表示画面に投稿されたものが表示されているか' do
           expect(page).to have_content list.title
           expect(page).to have_content list.body
-          expect(page).to have_content list.img
+          # expect(page).to have_content list.img
         end
       end
     end
@@ -61,18 +61,20 @@ Rspec.describe List, "コントローラーのテスト" do
       end
       context '表示の確認' do
         it '削除リンクが存在しているか' do
-          destroy_link = find_all('a')[0]
-          expect(destroy_link.native.inner_text).to match(/destroy/i)
+          # destroy_link = find_all('a')[0]
+          # expect(destroy_link.native.inner_text).to match(/destroy/i)
+          expect(page).to have_link '削除'
         end
         it '編集リンクが存在しているか' do
-          edit_link = find_all('a')[1]
-          expect(edit_link.native.inner_text).to match(/edit/i)
+          # edit_link = find_all('a')[1]
+          # expect(edit_link.native.inner_text).to match(/edit/i)
+          expect(page).to have_link '編集'
         end
       end
       context 'リンクの遷移先は編集画面か' do
-        edit_link = find_all('a')[1]
+        edit_link = find_all('a')[3] #1⇒3に修正
         edit_link.click
-        expect(current_path).to eq('/lists' + list.id.to_s + '/edit')
+        expect(current_path).to eq('/lists/' + list.id.to_s + '/edit') #'/lists'から'/lists/'に修正
       end
       context 'list削除のテスト' do
         it 'listの削除' do
@@ -89,7 +91,7 @@ Rspec.describe List, "コントローラーのテスト" do
         it '編集前のタイトルと本文がフォームに表示（セット）されている' do
           expect(page).to have_filed 'list[title]', whth: list.title
           expect(page).to have_filed 'list[body]', whth: list.body
-          expect(page).to have_filed 'list[body]', whth: list.img
+          # expect(page).to have_filed 'list[body]', whth: list.img
         end
         it '保存ボタンが表示される' do
           expect(page).to have_button '保存'
@@ -97,11 +99,11 @@ Rspec.describe List, "コントローラーのテスト" do
       end
       context '更新処理に関するテスト' do
         it '更新後のリダイレクト先は正しいか' do
-          fill_in 'list[itle]', with: Facker::Lorem.characters(number:5)
-          fill_in 'list[body]', with: Facker::Lorem.characters(number:20)
-          fill_in 'list[img]'
+          fill_in 'list[itle]', with: Facker::Lorem.characters(number:10) #number:5⇒10に修正
+          fill_in 'list[body]', with: Facker::Lorem.characters(number:30) #number:20⇒30に修正
+          # fill_in 'list[img]'
           click_button '保存'
-          expect(page).to have_current_path list_path(List.last)
+          expect(page).to have_current_path list_path(list) #List.last⇒listに修正
         end
       end
     end
