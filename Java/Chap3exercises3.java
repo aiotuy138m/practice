9269b78eaff722c1a933e18cd9eee77005e17885
aiotuy138m <122207424+aiotuy138m@users.noreply.github.com>
2023-08-08 14:59:55 +0000

    [ans]Java3章-3回答1代入

--- Java/Chap3exercises3.java
+++ Java/Chap3exercises3.java
@@ -1,6 +1,6 @@
 public class Chap3exercises3 {
   public static void main(String[] args) {
-    int isHungry = 0; //①intt型の変数isHungryを定義し0か1を代入、またString型の変数foodを定義し、適当な食べ物の名前を代入
+    int isHungry = 1; //①intt型の変数isHungryを定義し0か1を代入、またString型の変数foodを定義し、適当な食べ物の名前を代入
     String food = "apple";
     System.out.println("こんにちは"); //②画面に「こんにちは」と表示する
     if (isHungry == 0) { //③もし変数isHungryが0であれば「お腹がいっぱいです」、そうでなければ「はらぺこです」と表示する
