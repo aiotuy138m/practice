7d4e912ddd62f494af3de9992fe26e04c74be402
aiotuy138m <122207424+aiotuy138m@users.noreply.github.com>
2023-08-09 10:38:09 +0000

    [ans]Java3章-4回答

---
+++ Java/Chap3exercises4.java
@@ -0,0 +1,12 @@
+public class Chap3exercises4 {
+  public static void main(String[] args) {
+    boolean tenki = true;
+    if (tenki == true) {
+      System.out.println("洗濯をします");
+      System.out.println("散歩に行きます");
+    } else { //elseの後の波括弧がなかった。
+    System.out.println("DVDをみます");
+    System.out.println("寝ます");
+    }
+  }
+}
