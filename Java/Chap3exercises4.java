ecd1af3ba068bc320971d46ad8a4e3cbcc4c9896
aiotuy138m <122207424+aiotuy138m@users.noreply.github.com>
2023-08-09 11:33:46 +0000

    [add]Java3章-4解説追記

--- Java/Chap3exercises4.java
+++ Java/Chap3exercises4.java
@@ -4,7 +4,7 @@ public class Chap3exercises4 {
     if (tenki == true) {
       System.out.println("洗濯をします");
       System.out.println("散歩に行きます");
-    } else { //elseの後の波括弧がなかった。
+    } else { //elseの後の波括弧がなかった。追記：１文だけの場合は波括弧を省略できるため、DVDの記述でelse文が終了扱いになってtrueでも寝ますが出力
     System.out.println("DVDをみます");
     System.out.println("寝ます");
     }
