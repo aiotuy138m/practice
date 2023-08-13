1850406043c73594e9cd980d4dece22e1d5f357b
aiotuy138m <122207424+aiotuy138m@users.noreply.github.com>
2023-08-13 14:00:50 +0000

    [fix]Java4章練習問題

--- Java/Chap4exercises4.java
+++ Java/Chap4exercises4.java
@@ -1,13 +1,14 @@
 public class Chap4exercises4 {
   public static void main(String[] args) {
-    int[] numbers = {3, 4, 9};
-    System.out.println("1桁の数字を入力してください");
-    int input = new java.util.Scanner(System.in).nextInt();
-    for (int hit :numbers) {
+    int[] numbers = {3, 4, 9}; //①int型で要素数3の配列nuumbersを準備、この時の初期値は「3」「4」「9」
+    System.out.println("1桁の数字を入力してください"); //②画面に「1桁の数字を入力してください」と表示
+    int input = new java.util.Scanner(System.in).nextInt(); //③←を用いてキーボードから数字の入力をつけつけ、変数inputに代入
+    for (int hit :numbers) { //配列をループで回しながら、いずれかの要素と等しいかを調べる。もし等しければ「アタリ！」と表示
       if (input == hit) {
         System.out.println("アタリ！");
-      } else {
-        System.out.println("ハズレ");
+        //はすれパターンの記載不要だったので修正
+      // } else {
+        // System.out.println("ハズレ");
       }
     }
   }
