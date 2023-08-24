ea3fd913dc4d70f2da60b40e079204597b10bb2e
aiotuy138m <122207424+aiotuy138m@users.noreply.github.com>
2023-08-24 14:56:37 +0000

    Java6章　import文使用前

--- Java/Chap6/Calc.java
+++ Java/Chap6/Calc.java
@@ -1,10 +1,14 @@
+package calcapp.main; //パッケージ
 public class Calc {
   public static void main(String[] args) {
     int a = 10; int b = 2;
-    int total = CalcLogic.tasu(a, b); //ただtasuだけだと別のクラスで記載しているものだから引っ張って来れない
-    int delta = CalcLogic.hiku(a, b);
+    int total = calcapp.logics.CalcLogic.tasu(a, b); //ただtasuだけだと別のクラスで記載しているものだから引っ張って来れない完全限定クラス名または完全修飾クラス名（FQCN）で書く
+    int delta = calcapp.logics.CalcLogic.hiku(a, b);
     System.out.println("足すと" + total + "、引くと" + delta);
   }
 }
 
-// メインをコンパイルしただけだけど、CalcLogicの方もコンパイルされた。使用するものは全てコンパイルされるっぽい
+// メインをコンパイルしただけだけど、CalcLogicの方もコンパイルされた。使用するものは全てコンパイルされるっぽい(cloud9の使用？JDKではそれぞれコンパイルする必要があるみたい)
+//パッケージの名前はJavaの識別子のルールに従ってれば自由だが、アルファベットは小文字にするのが一般的で、上記のようなドットで区切ったパッケージ名も多く用いられる
+//パッケージに親子関係や階層関係はない
+//FQCNはimport文を使うことによって手間を削減できる
