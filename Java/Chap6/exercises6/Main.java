5d454121b7d5135fad11518f358a507b133a80f9
aiotuy138m <122207424+aiotuy138m@users.noreply.github.com>
2023-08-25 05:19:36 +0000

    [finish]Java6章練習問題6-1

---
+++ Java/Chap6/exercises6/Main.java
@@ -0,0 +1,9 @@
+import comment.Zenhan
+public class Main {
+  public static void main(String[] args) throws Exception {
+    doWarusa();
+    doTogame();
+    callDeae();
+    showMondokoro();
+  }
+}
