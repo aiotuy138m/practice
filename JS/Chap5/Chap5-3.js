f64487de231b33098a31cf17d5f22eb9c62d20ab
aiotuy138m <122207424+aiotuy138m@users.noreply.github.com>
2023-08-29 06:38:17 +0000

    [add]JS5章表追加機能

---
+++ JS/Chap5/Chap5-3.js
@@ -0,0 +1,17 @@
+const table = document.querySelector('table');
+const authipt = document.querySelector('#auth');
+const bookipt = document.querySelector('#book');
+const btn = document.querySelector('button');
+btn.addEventListener('click', () => {
+  if(authipt.value && bookipt.value){
+    const tr = document.createElement('tr');
+    const td1 = document.createElement('td');
+    const td2 = document.createElement('td');
+    td1.innerText = authipt.value;
+    td2.innerText = bookipt.value;
+    tr.appendChild(td1);
+    tr.appendChild(td2);
+    table.appendChild(tr);
+  }
+});
+// if文を入れることで両方に入力のない場合は追加されない
