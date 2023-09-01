af9fe2d191e00aa1396725a280c816d320b9bc67
aiotuy138m <122207424+aiotuy138m@users.noreply.github.com>
2023-09-01 02:06:23 +0000

    [add]JS6章chap6-3.jsの解説と追記

--- JS/Chap6/chap6-3.js
+++ JS/Chap6/chap6-3.js
@@ -6,8 +6,9 @@ const callApi = async () => {
   url.searchParams.set('q', keyword.value);
   const res = await fetch(url);
   const books = await res.json();
-  for (const book of books.items) {
-    const b = book.volumeInfo;
+  //ここからchap6-2と違うところ。今回はWebページに引っ張ってきたAPIの内容を表示させる
+  for (const book of books.items) { //変数booksのitemプロパティに所属する要素を、新規作成した変数bookに順次入れる間、以下を繰り返せ
+    const b = book.volumeInfo; //変数bookのvolumeInfoプロパティの値を新規作成した変数bに入れろ
     const tr = document.createElement('tr');
     const td1 = document.createElement('td');
     const td2 = document.createElement('td');
