fdf5807067570f62beb1a465773d0f3d0881464b
aiotuy138m <122207424+aiotuy138m@users.noreply.github.com>
2023-09-11 12:46:38 +0000

    [add]Java8章Matangoクラスの作成

--- Java/Chap8/Hero.java
+++ Java/Chap8/Hero.java
@@ -1,7 +1,8 @@
 public class Hero {
   //属性の定義
-  String name;
-  int hp;
+  String name; //名前の宣言
+  int hp; //HPの宣言
+  //上記の記述で2つのフィールドが宣言された
   //操作の定義
   public void attack() {}
   public void sleep() {}
