a73f52ff6cb2cbf977fb558d149a9fd90a1ed158
aiotuy138m <122207424+aiotuy138m@users.noreply.github.com>
2023-09-14 04:04:46 +0000

    [add]Java8章Main.java記述＆実行

--- Java/Chap8/main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-  public static void main(String[] args) {
-    //（以下の内容をJavaで記述していく）
-    //勇者よ、この可能世界に生まれよ！
-    //お化けキノコよ、この仮想世界に生まれよ！
-    //勇者よ、戦え！
-    //お化けキノコよ、逃げろ！
-  }
-}
