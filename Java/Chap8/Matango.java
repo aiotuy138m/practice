fc5293d41df7aaea357399d9720d82cbb29ada1b
aiotuy138m <122207424+aiotuy138m@users.noreply.github.com>
2023-09-15 12:23:28 +0000

    [add]Java8章Matango.javaとMain.java記述＆実行

--- Java/Chap8/Matango.java
+++ Java/Chap8/Matango.java
@@ -1,4 +1,8 @@
 public class Matango {
   int hp;
   final int LEVEL = 10;  //お化けキノコのレベルに初期値10を設定
+  char suffix;
+  public void run() {
+    System.out.println("お化けキノコ" + this.suffix + "は逃げ出した！");
+  }
 }
